1a7084d0c9582f91f395ea2aab6eaf2bb68f8215
Eric Fossum <fossum.eric@gmail.com>
2021-02-08 10:33:39 -0800

    Incrementing version.
    Adding build artifacts to gitignore.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flowroute-messaging-python',
-    version='0.0.0',
+    version='0.1.0',
     license='MIT',
     description='Flowroute\'s Messaging API',
     author='Flowroute Developers',
